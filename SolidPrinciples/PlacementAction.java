b0a81a5d79f5c8ee6e8f54e2485172d10d445c82
Angelica Mae Manliguez <142959759+AngelicaManliguez@users.noreply.github.com>
2024-05-06 10:43:34 +0800

    Update PlacementAction.java

--- SolidPrinciples/PlacementAction.java
+++ SolidPrinciples/PlacementAction.java
@@ -3,7 +3,6 @@ package SolidPrinciples;
 public class PlacementAction implements OrderPlacement {
     @Override
     public void placeOrder(String customerName, String address) {
-        // Simulate placing order in a system
         System.out.println("Order placed for " + customerName + " at " + address);
     }
 }
