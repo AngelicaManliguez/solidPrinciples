d3811ab3bca794d2af31cbd77f40b08ee58c0e79
Angelica Mae Manliguez <142959759+AngelicaManliguez@users.noreply.github.com>
2024-05-06 11:47:15 +0800

    Update Order.java

--- SolidPrinciples/Order.java
+++ SolidPrinciples/Order.java
@@ -1,18 +1,18 @@
 package SolidPrinciples;
 
 public class Order {
-    private final OrderCalculator orderCalculation;
+    private final OrderCalculator orderCalculator;
     private final OrderPlacement orderPlacement;
     private final OrderInvoice orderInvoice;
 
     public Order(OrderCalculator orderCalculator, OrderPlacement orderPlacement, OrderInvoice orderInvoice) {
-        this.orderCalculation = orderCalculator;
+        this.orderCalculator = orderCalculator;
         this.orderPlacement = orderPlacement;
         this.orderInvoice = orderInvoice;
     }
 
     public void orderWithoutInvoice(String customerName, String address, double price, int quantity) {
-        orderCalculation.calculateTotal(price,quantity);
+        orderCalculator.calculateTotal(price,quantity);
         orderPlacement.placeOrder(customerName, address);
     }
 
